315ded3b849fa0bb844e25e2048a43ee3458a118
Vladislav Bryl <adwasarik@gmail.com>
2025-04-26 11:31:38 +0300

    feat: implement rename function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,18 +2,18 @@ import fs from 'fs/promises'
 import path from 'path'
 
 const copy = async () => {
-  const sourceFolder = path.join('src', 'fs', 'files')
-  const targetFolder = path.join('src', 'fs', 'files_copy')
+  const pathToFolder = path.join('src', 'fs', 'files')
+  const pathToCopiedFolder = path.join('src', 'fs', 'files_copy')
 
   try {
-    const sourceStats = await fs.stat(sourceFolder)
+    const sourceStats = await fs.stat(pathToFolder)
 
     if (!sourceStats.isDirectory()) {
       throw new Error('FS operation failed')
     }
 
     try {
-      await fs.access(targetFolder)
+      await fs.access(pathToCopiedFolder)
       throw new Error('FS operation failed')
     } catch (error) {
       if (error.code !== 'ENOENT') {
@@ -21,13 +21,13 @@ const copy = async () => {
       }
     }
 
-    await fs.mkdir(targetFolder)
+    await fs.mkdir(pathToCopiedFolder)
 
-    const files = await fs.readdir(sourceFolder)
+    const files = await fs.readdir(pathToFolder)
 
     for (const file of files) {
-      const sourcePath = path.join(sourceFolder, file)
-      const targetPath = path.join(targetFolder, file)
+      const sourcePath = path.join(pathToFolder, file)
+      const targetPath = path.join(pathToCopiedFolder, file)
       const stats = await fs.stat(sourcePath)
 
       if (stats.isDirectory()) {
@@ -43,7 +43,7 @@ const copy = async () => {
 
 async function copyDirectory(source, target) {
   await fs.mkdir(target)
-  
+
   const files = await fs.readdir(source)
 
   for (const file of files) {
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
-const rename = async () => {
-    // Write your code here 
-};
+import fs from 'fs/promises'
+import path from 'path'
 
-await rename();
+const rename = async () => {
+  const pathToFile = path.join('src', 'fs', 'files', 'wrongFilename.txt')
+  const newPathToFile = path.join('src', 'fs', 'files', 'properFilename.md')
+
+  try {
+    await fs.access(pathToFile)
+
+    try {
+      await fs.access(newPathToFile)
+      throw new Error('FS operation failed')
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw new Error('FS operation failed')
+      }
+    }
+
+    await fs.rename(pathToFile, newPathToFile)
+  } catch (error) {
+    throw new Error('FS operation failed')
+  }
+}
+
+await rename()
