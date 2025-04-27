a7fdf656d040983f2e5477d307b9a3dce91838b1
Vladislav Bryl <adwasarik@gmail.com>
2025-04-27 11:34:06 +0300

    feat: iplement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
-const list = async () => {
-    // Write your code here 
-};
+import fs from 'fs/promises'
+import path from 'path'
 
-await list();
+const list = async () => {
+  const pathToFolder = path.join('src', 'fs', 'files')
+
+  try {
+    const files = await fs.readdir(pathToFolder)
+    console.log(files)
+  } catch (error) {
+    throw new Error('FS operation failed')
+  }
+}
+
+await list()
