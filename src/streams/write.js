d7e084145a324760cb5615a81def0461cfd0f2f3
Vladislav Bryl <adwasarik@gmail.com>
2025-04-28 12:49:47 +0300

    feat: implement write stream funciotn

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+
 const write = async () => {
-    // Write your code here 
+  const pathToFile = path.join('src', 'streams', 'files', 'fileToWrite.txt')
+  const writableStream = fs.createWriteStream(pathToFile);
+
+  process.stdin.pipe(writableStream);
+
 };
 
 await write();
