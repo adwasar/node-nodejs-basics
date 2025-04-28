41caa84c1e72625ad61c1aa8a87948ebc1c7277d
Vladislav Bryl <adwasarik@gmail.com>
2025-04-28 13:09:14 +0300

    feat: implement compress function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,12 +1,11 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
 const write = async () => {
   const pathToFile = path.join('src', 'streams', 'files', 'fileToWrite.txt')
-  const writableStream = fs.createWriteStream(pathToFile);
+  const writableStream = fs.createWriteStream(pathToFile)
 
-  process.stdin.pipe(writableStream);
+  process.stdin.pipe(writableStream)
+}
 
-};
-
-await write();
+await write()
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
-const compress = async () => {
-    // Write your code here 
-};
+import fs from 'fs'
+import zlib from 'zlib'
+import path from 'path'
 
-await compress();
+const compress = async () => {
+  const inputFilePath = path.join('src', 'zip', 'files', 'fileToCompress.txt')
+  const outputFilePath = path.join('src', 'zip', 'files', 'archive.gz')
+
+  const readStream = fs.createReadStream(inputFilePath)
+  const gzipStream = zlib.createGzip()
+  const writeStream = fs.createWriteStream(outputFilePath)
+
+  readStream.pipe(gzipStream).pipe(writeStream)
+}
+
+await compress()
