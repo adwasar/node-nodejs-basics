bd40ab98e0ae9802d9ee877e1c1421006aa17716
Vladislav Bryl <adwasarik@gmail.com>
2025-04-28 12:19:34 +0300

    feat: implement caclHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = path.join('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    const stream = createReadStream(pathToFile);
+    
+    return new Promise((resolve, reject) => {
+        stream.on('data', (chunk) => hash.update(chunk));
+        stream.on('end', () => {
+            const hexHash = hash.digest('hex');
+            console.log(hexHash);
+            resolve(hexHash);
+        });
+        stream.on('error', reject);
+    });
 };
 
 await calculateHash();
